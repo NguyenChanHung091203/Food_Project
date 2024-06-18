c79ff618b9fd9fcf6360c523d4176a5b173f42d9
DươngLondon <ndd050903@gmail.com>
2024-06-18 23:42:27 +0700

    Food_Shop

--- app/src/main/java/com/example/food_project/Activity/LoginActivity.java
+++ app/src/main/java/com/example/food_project/Activity/LoginActivity.java
@@ -42,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         binding.txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(LoginActivity.this,MainActivity.class);
+                Intent i = new Intent(LoginActivity.this,SignupActivity.class);
                 startActivity(i);
             }
         });
